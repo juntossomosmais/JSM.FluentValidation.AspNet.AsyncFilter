d7cf0e5f2c8a26cdd8a4960fc96187f3d87eec27
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-11-19 10:13:18 -0300

    build: add dotnet 2.2 to actions

--- src/ModelValidationOptions.cs
+++ src/ModelValidationOptions.cs
@@ -2,6 +2,10 @@ namespace FluentValidation.AspNet.AsyncValidationFilter
 {
     public class ModelValidationOptions
     {
+        /// <summary>
+        /// Determines if the filter will be applied only to controllers with the
+        /// ApiControllerAttribute
+        /// </summary>
         public bool OnlyApiController { get; set; } = false;
     }
 }
